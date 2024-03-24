dadd2aa8bfe6a82fc8a4f24e70a49dfb74e18acd
Jonathan Thomas <jonathan@openshot.org>
2024-03-23 21:01:28 -0500

    Inject Mixin to hide custom name from appearing on Entity. We will be drawing the custom name ourselves.

---
+++ src/client/java/com/owlmaddie/mixin/client/EntityRendererMixin.java
@@ -0,0 +1,20 @@
+package com.owlmaddie.mixin.client;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderer.class)
+public abstract class EntityRendererMixin<T extends Entity> {
+
+    @Inject(method = "renderLabelIfPresent", at = @At("HEAD"), cancellable = true)
+    private void cancelRenderLabel(T entity, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        ci.cancel(); // Hide Entity Custom Names
+    }
+}
