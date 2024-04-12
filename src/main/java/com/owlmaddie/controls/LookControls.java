ce7ba7b616f56b754658ebebeefe2b7667c506b2
Jonathan Thomas <jonathan@openshot.org>
2024-04-11 19:42:00 -0500

    Added FlyingEntity support to look controls, fixes Ghast and Phantom

--- src/main/java/com/owlmaddie/controls/LookControls.java
+++ src/main/java/com/owlmaddie/controls/LookControls.java
@@ -1,26 +1,25 @@
 package com.owlmaddie.controls;
 
+import net.minecraft.entity.mob.FlyingEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.SlimeEntity;
 import net.minecraft.entity.passive.SquidEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * The {@code LookControls} class enables entities to look at the player,
  * with specific adjustments for certain mobs like Slimes and Squids.
  */
 public class LookControls {
-    public static final Logger LOGGER = LoggerFactory.getLogger("mobgpt");
-
     public static void lookAtPlayer(ServerPlayerEntity player, MobEntity entity) {
         if (entity instanceof SlimeEntity) {
             handleSlimeLook((SlimeEntity) entity, player);
         } else if (entity instanceof SquidEntity) {
             handleSquidLook((SquidEntity) entity, player);
+        } else if (entity instanceof FlyingEntity) {
+            handleFlyingEntity((FlyingEntity) entity, player);
         } else {
             // Make the entity look at the player
             entity.getLookControl().lookAt(player, 10.0F, (float)entity.getMaxLookPitchChange());
@@ -45,6 +44,33 @@ public class LookControls {
         if (distanceToPlayer < 3.5F) {
             // Stop motion when close
             squid.setVelocity(0,0,0);
+        }
+    }
+
+    // Ghast, Phantom, etc...
+    private static void handleFlyingEntity(FlyingEntity flyingEntity, ServerPlayerEntity player) {
+        Vec3d playerPosition = player.getPos();
+        Vec3d flyingPosition = flyingEntity.getPos();
+        Vec3d toPlayer = playerPosition.subtract(flyingPosition).normalize();
+
+        // Calculate the yaw to align the flyingEntity's facing direction with the movement direction
+        float targetYaw = (float)(MathHelper.atan2(toPlayer.z, toPlayer.x) * (180 / Math.PI) - 90);
+        flyingEntity.setYaw(targetYaw);
+
+        // Look at player while adjusting yaw
+        flyingEntity.getLookControl().lookAt(player, 10.0F, (float)flyingEntity.getMaxLookPitchChange());
+
+        float initialSpeed = 0.15F;
+        flyingEntity.setVelocity(
+                (float) toPlayer.x * initialSpeed,
+                (float) toPlayer.y * initialSpeed,
+                (float) toPlayer.z * initialSpeed
+        );
+
+        double distanceToPlayer = flyingEntity.getPos().distanceTo(player.getPos());
+        if (distanceToPlayer < 9F) {
+            // Stop motion when close
+            flyingEntity.setVelocity(0, 0, 0);
         }
     }
 
