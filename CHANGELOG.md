89935ff0f7fe8acd4e4ed2f40348062af2be852d
Jonathan Thomas <jonathan@openshot.org>
2024-07-06 22:01:27 -0500

    - Added support for non path aware entities to FLEE (i.e. Ghast) - Fixed a **crash with FLEE** when non-path aware entities (i.e. Ghast) attempted to flee.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable changes to **CreatureChat** are documented in this file. The format 
 [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to 
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Added
+- Added support for non path aware entities to FLEE (i.e. Ghast)
+
+### Changed
+- Fixed a **crash with FLEE** when non-path aware entities (i.e. Ghast) attempted to flee.
+
 ## [1.0.7] - 2024-07-03
 
 ### Added
--- src/main/java/com/owlmaddie/goals/FleePlayerGoal.java
+++ src/main/java/com/owlmaddie/goals/FleePlayerGoal.java
@@ -43,14 +43,29 @@ public class FleePlayerGoal extends PlayerBaseGoal {
 
     private void fleeFromPlayer() {
         int roundedFleeDistance = Math.round(fleeDistance);
-        Vec3d fleeTarget = FuzzyTargeting.findFrom((PathAwareEntity)this.entity, roundedFleeDistance,
-                roundedFleeDistance, this.targetEntity.getPos());
+        if (this.entity instanceof PathAwareEntity) {
+            // Set random path away from player
+            Vec3d fleeTarget = FuzzyTargeting.findFrom((PathAwareEntity) this.entity, roundedFleeDistance,
+                    roundedFleeDistance, this.targetEntity.getPos());
 
-        if (fleeTarget != null) {
-            Path path = this.entity.getNavigation().findPathTo(fleeTarget.x, fleeTarget.y, fleeTarget.z, 0);
-            if (path != null) {
-                this.entity.getNavigation().startMovingAlong(path, this.speed);
+            if (fleeTarget != null) {
+                Path path = this.entity.getNavigation().findPathTo(fleeTarget.x, fleeTarget.y, fleeTarget.z, 0);
+                if (path != null) {
+                    this.entity.getNavigation().startMovingAlong(path, this.speed);
+                }
             }
+
+        } else {
+            // Move in the opposite direction from player (for non-path aware entities)
+            Vec3d playerPos = this.targetEntity.getPos();
+            Vec3d entityPos = this.entity.getPos();
+
+            // Calculate the direction away from the player
+            Vec3d fleeDirection = entityPos.subtract(playerPos).normalize();
+
+            // Apply movement with the entity's speed in the opposite direction
+            this.entity.setVelocity(fleeDirection.x * this.speed, this.entity.getVelocity().y, fleeDirection.z * this.speed);
+            this.entity.velocityModified = true;
         }
     }
 
