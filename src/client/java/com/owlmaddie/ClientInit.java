84238ed524cbfe4f93ad8edd24f2256da409211b
Jonathan Thomas <jonathan@openshot.org>
2025-06-27 15:57:23 -0500

    Creating TickDelta helper, to allow overriding

--- src/client/java/com/owlmaddie/ClientInit.java
+++ src/client/java/com/owlmaddie/ClientInit.java
@@ -11,6 +11,7 @@ import com.owlmaddie.particle.Particles;
 import com.owlmaddie.ui.BubbleRenderer;
 import com.owlmaddie.ui.ClickHandler;
 import com.owlmaddie.ui.PlayerMessageManager;
+import com.owlmaddie.utils.TickDelta;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
@@ -50,8 +51,9 @@ public class ClientInit implements ClientModInitializer {
         ClientPackets.register();
 
         // Register an event callback to render text bubbles
-        WorldRenderEvents.BEFORE_DEBUG_RENDER.register((context) -> {
-            BubbleRenderer.drawTextAboveEntities(context, tickCounter, context.tickDelta());
+        WorldRenderEvents.BEFORE_DEBUG_RENDER.register(ctx -> {
+            float delta = TickDelta.get(ctx);
+            BubbleRenderer.drawTextAboveEntities(ctx, tickCounter, delta);
         });
 
         // Register an event callback for when the client disconnects from a server or changes worlds
