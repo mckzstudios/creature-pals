88185261afb720c53888bf8468e895c7b8584a14
lorisj-elefant <loris@elefant.gg>
2025-07-22 09:15:39 -0700

    fix playerdata

--- src/main/java/com/owlmaddie/chat/PlayerData.java
+++ src/main/java/com/owlmaddie/chat/PlayerData.java
@@ -2,9 +2,6 @@ package com.owlmaddie.chat;
 
 import com.google.gson.annotations.Expose;
 
-import net.minecraft.network.RegistryByteBuf;
-import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.codec.PacketCodecs;
 
 /**
  * The {@code PlayerData} class represents data associated with a player,
